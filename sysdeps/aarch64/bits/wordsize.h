373727165d18971e9f1846ee4f398973faa25598
Andrew Pinski <apinski@cavium.com>
2014-04-18 13:34:36 -0700

    2014-04-18  Andrew Pinski  <apinski@cavium.com>
    	* sysdeps/aarch64/bits/wordsize.h (__WORDSIZE_TIME64_COMPAT32): Define
    	unconditionally.

--- sysdeps/aarch64/bits/wordsize.h
+++ sysdeps/aarch64/bits/wordsize.h
@@ -2,7 +2,9 @@
 
 #ifdef __LP64__
 # define __WORDSIZE	64
-# define __WORDSIZE_TIME64_COMPAT32	1
 #else
 # define __WORDSIZE	32
 #endif
+
+/* Both ILP32 and LP64 have a 64bit time_t. */
+#define __WORDSIZE_TIME64_COMPAT32	1
