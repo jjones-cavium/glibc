a78db6b16b83743fbedc4a6e0eaa288fd48bdcff
Andrew Pinski <apinski@cavium.com>
2014-04-27 23:56:12 -0700

    2014-04-27  Andrew Pinski  <apinski@cavium.com>
    	Bug #10393
    	* sysdeps/unix/sysv/linux/aarch64/sigaction.c: Fix copyright header.
    	(__libc_sigaction): Handle kernel_sigaction where the fields are
    	unsigned long long in type so that ILP32 structure layout exactly
    	matches LP64's.
    	Also the syscall will never return ENOSYS so delete that part of the code.
    	* sysdeps/unix/sysv/linux/aarch64/kernel_sigaction.h: New header.

--- sysdeps/unix/sysv/linux/aarch64/sigaction.c
+++ sysdeps/unix/sysv/linux/aarch64/sigaction.c
@@ -1,11 +1,10 @@
 /* Copyright (C) 1997-2012 Free Software Foundation, Inc.
-
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public License as
-   published by the Free Software Foundation; either version 2.1 of the
-   License, or (at your option) any later version.
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
    The GNU C Library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -36,34 +35,29 @@ int
 __libc_sigaction (int sig, const struct sigaction *act, struct sigaction *oact)
 {
   int result;
-  struct kernel_sigaction kact;
-  struct kernel_sigaction koact;
+
+  struct kernel_sigaction kact, koact;
 
   if (act)
     {
-      kact.k_sa_handler = act->sa_handler;
+      kact.k_sa_handler = (unsigned long long)(uintptr_t)act->sa_handler;
       memcpy (&kact.sa_mask, &act->sa_mask, sizeof (sigset_t));
       kact.sa_flags = act->sa_flags;
-#ifdef HAVE_SA_RESTORER
       if (kact.sa_flags & SA_RESTORER)
-	kact.sa_restorer = act->sa_restorer;
-#endif
+	kact.sa_restorer = (unsigned long long)(uintptr_t)act->sa_restorer;
     }
 
+  /* This is needed for ILP32 as the structures are two different sizes due to
+     using the LP64 structure.  */
   result = INLINE_SYSCALL (rt_sigaction, 4, sig,
 			   act ? __ptrvalue (&kact) : NULL,
 			   oact ? __ptrvalue (&koact) : NULL, _NSIG / 8);
-  if (result >= 0 || errno != ENOSYS)
+  if (oact && result >= 0)
     {
-      if (oact && result >= 0)
-	{
-	  oact->sa_handler = koact.k_sa_handler;
-	  memcpy (&oact->sa_mask, &koact.sa_mask, sizeof (sigset_t));
-	  oact->sa_flags = koact.sa_flags;
-#ifdef HAVE_SA_RESTORER
-	  oact->sa_restorer = koact.sa_restorer;
-#endif
-	}
+      oact->sa_handler = (void*)(uintptr_t)koact.k_sa_handler;
+      memcpy (&oact->sa_mask, &koact.sa_mask, sizeof (sigset_t));
+      oact->sa_flags = koact.sa_flags;
+      oact->sa_restorer = (void*)(uintptr_t)koact.sa_restorer;
     }
   return result;
 }
